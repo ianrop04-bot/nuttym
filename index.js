f6b0c413b495a8169da9bdeba3b5ac07d4df9a54
prof-K <ianrop04@gmail.com>
2026-09-07 17:50:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const path= require('path')
 const app = express()
 app.use(cors())
 
-app.use(express.static('public'))
+
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'src/index.html'))
+  res.sendFile(path.join(__dirname,'./index.html'))
 })
 app.get('/git',(req,res)=>{
-  res.sendFile(path.join(__dirname,'src/hub.html'))
+  res.sendFile(path.join(__dirname,'./hub.html'))
 })
 app.get('/altechai',(req, res)=>{
-  res.sendFile(path.join(__dirname,'src/ai.html'))
+  res.sendFile(path.join(__dirname,'ai.html'))
 })
 app.get('/wabot',(req,res)=>{
-  res.sendFile(path.join(__dirname,'src/pair.html'))
+  res.sendFile(path.join(__dirname,'pair.html'))
 })
 
 app.listen('3098',()=>{console.log('Working Tool ...')})
